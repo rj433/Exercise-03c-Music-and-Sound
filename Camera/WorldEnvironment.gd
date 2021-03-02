877d322063d39b4ed7b82c90ad84ed1602b58893
rj433 <rjanoski@iu.edu>
2021-03-02 02:33:49 -0500

    Completes exercise

--- Camera/WorldEnvironment.gd
+++ Camera/WorldEnvironment.gd
@@ -7,9 +7,9 @@ func _ready():
 
 func show():
 	if HUD.effects_blur:
-		pass
+		environment.background_mode = Environment.BG_CANVAS
 	else:
-		pass
+		environment.background_mode = Environment.BG_CLEAR_COLOR
 
 func hide():
 	pass
